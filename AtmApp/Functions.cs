5a8b7e3200a400c1c361018584301c254283c73e
Zeketo1 <152497390+Zeketo1@users.noreply.github.com>
2024-03-07 22:13:52 +0100

    Add project files.

---
+++ AtmApp/Functions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmApp
+{
+    internal class Functions
+    {
+        Random random = new Random();
+
+        int accountBalance = 20000;
+        int accountPin = 1234;
+
+        public Functions()
+        {
+        }
+
+        public void checkBalance()
+        {
+            Console.WriteLine("Your account balance is: {0}", accountBalance);
+            Console.WriteLine("Press Enter key to return to the home page");
+            Console.ReadKey();
+        }
+
+        public void depositFunds()
+        {
+            Console.WriteLine("How much do you want to Deposit?");
+            string inputAmount = Console.ReadLine();
+            int depositAmount = Convert.ToInt32(inputAmount);
+
+            accountBalance += depositAmount;
+
+            Console.WriteLine("Your new balance is: {0}", accountBalance);
+            Console.WriteLine("Press Enter key to return to the home page");
+            Console.ReadKey();
+        }
+
+        public void withdraw()
+        {
+            Console.WriteLine("How much do you want to Withdraw?");
+            string inputAmount = Console.ReadLine();
+            int withdrawAmount = Convert.ToInt32(inputAmount);
+            int condition = 0;
+            int trys = 2;
+
+
+            while(condition < 3)
+            {
+                Console.Write("Enter your account pin: ");
+                string pin = Console.ReadLine();
+                int pin2 = Convert.ToInt16(pin);
+                if (pin2 == accountPin)
+                {
+                    accountBalance -= withdrawAmount;
+                    Console.WriteLine("Your new balance is: {0}", accountBalance);
+                    Console.WriteLine("Press Enter key to return to the home page");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    if (condition == 2)
+                    {
+                        Console.WriteLine("Too many Invalid attempts.....");
+                        Console.WriteLine("Withdrawal Failed Thieffffffffff.....");
+                        break;
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Invalid Pin.......Re-enter PIN.......You have {0} more trys", trys--);
+                        condition++;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ AtmApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AtmApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions fun = new Functions();
+
+            bool condition = true;
+
+            while(condition)
+            {
+                Console.WriteLine("1. Check account Balance");
+                Console.WriteLine("2. Deposit Funds");
+                Console.WriteLine("3. Withdraw Cash");
+                Console.WriteLine("4. Transfer Money");
+
+                // Read the user's choice
+                string input = Console.ReadLine();
+                int choice = Convert.ToInt32(input);
+
+                switch (choice)
+                {
+                    case 1:
+                        fun.checkBalance();
+                        break;
+                    case 2:
+                        fun.depositFunds();
+                        break;
+                    case 3:
+                        fun.withdraw();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+}   }
